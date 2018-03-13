467fd381f6dd25ace13b223dc437a50560765317
Enrique Sánchez <enrique.sanchez@cabify.com>
2018-03-13 21:07:52 +0100

    Added a mock example

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from sample.strings_example import StringsExamples
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    print StringsExamples.concat_strings(args[0], args[1])
+    print("Concat strings %s %s..." % (args[0], args[1]))
+    concated_strings = StringsExamples.concat_strings(args[0], args[1])
--- tests/test_strings_examples.py
+++ tests/test_strings_examples.py
@@ -1,6 +1,6 @@
 import unittest
 from sample.strings_example import StringsExamples
-
+from mock import patch, Mock
 
 class TestStringsExamples(unittest.TestCase):
     def test_concat_strings(self):
@@ -8,6 +8,29 @@ class TestStringsExamples(unittest.TestCase):
         string2 = "adios"
         result = StringsExamples.concat_strings(string1, string2)
         assert result == "holaadios"
+
+    def test_concat_numbers(self):
+        string1 = 123
+        string2 = 456
+        self.assertRaises(TypeError, StringsExamples.concat_strings, string1, string2)
+
+    def test_concat_numbers_string(self):
+        string1 = 123
+        string2 = "asd"
+        self.assertRaises(TypeError, StringsExamples.concat_strings, string1, string2)
+
+    def test_concat_string_numbers(self):
+        string1 = "asd"
+        string2 = 123
+        self.assertRaises(TypeError, StringsExamples.concat_strings, string1, string2)
+
+    @patch('sample.strings_example.urllib2.urlopen')
+    def test_get_first_string_from_internet(self, mock_urlopen):
+        a = Mock()
+        a.read.side_effect = ["hello good bye"]
+        mock_urlopen.return_value = a
+        assert StringsExamples.get_first_string() == "hello"
+
 
 
 if __name__ == '__main__':
